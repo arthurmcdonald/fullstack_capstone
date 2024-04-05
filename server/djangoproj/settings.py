62b018106fd791e9bd49895a47b43be5069c5833
arthur <arthur@example.com>
2024-04-04 18:28:42 -0600

    fix: linting issues pass 4

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,12 +28,12 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-allowed_hosts_value = "https://arthuramcdon-8000.theianext-1-" +
-                      "labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai"
+allowed_hosts_value = ("https://arthuramcdon-8000.theianext-1-labs" +
+                       "-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai")
 ALLOWED_HOSTS = ['localhost', allowed_hosts_value]
 
-trusted_origins_value = "https://arthuramcdon-8000.theianext-1-labs" +
-                        "-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai"
+trusted_origins_value = ("https://arthuramcdon-8000.theianext-1-labs" +
+                         "-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai")
 CSRF_TRUSTED_ORIGINS = [trusted_origins_value]
 
 REST_FRAMEWORK = {
